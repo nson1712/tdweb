888409e96e34d4016ec529cab5b8851505cd34c3
khoitd <khoi.sm@ascertain.com.my>
2025-02-14 05:18:01 +0700

    update css

--- src/components/TopFull/index.jsx
+++ src/components/TopFull/index.jsx
@@ -3,7 +3,7 @@ import VerticalStoryItem from "../VerticalStoryItem/VerticalStoryItem";
 
 const TopFull = ({ data }) => {
   return (
-    <div className="grid grid-cols-4 justify-center md:grid-cols-8 md:grid-rows-2 gap-x-2 gap-y-6">
+    <div className="grid grid-cols-4 justify-center md:grid-cols-8 md:grid-rows-2 gap-x-4 gap-y-6">
       {data?.slice(0, 16).map((item, index) => (
         <VerticalStoryItem
           key={index}
