6a73fb6b98261bcad06a29560c2bb5d82d42706c
ADPC <phamngocson171201@gmail.com>
2025-02-20 16:26:14 +0700

    chore: add deposit diamond btn id

--- src/pages/StorySummary/StorySummary.js
+++ src/pages/StorySummary/StorySummary.js
@@ -788,7 +788,7 @@ const StorySummary = () => {
                   href={`/nap-kim-cuong?ref=${GlobalStore.profile?.referralCode}`}
                   passHref
                 >
-                  <a class="relative border-0 bg-transparent p-0 mt-4 cursor-pointer outline-none focus:outline-none select-none touch-manipulation transition-filter duration-250 group">
+                  <a id="nap-kim-cuong" class="relative border-0 bg-transparent p-0 mt-4 cursor-pointer outline-none focus:outline-none select-none touch-manipulation transition-filter duration-250 group">
                     <span class="absolute top-0 left-0 w-full h-full rounded-xl bg-black/25 will-change-transform translate-y-[2px] transition-transform duration-600 ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:translate-y-[4px] group-active:translate-y-[1px]"></span>
                     <span class="absolute top-0 left-0 w-full h-full rounded-xl bg-gradient-to-l from-[hsl(340deg_100%_16%)] via-[hsl(340deg_100%_32%)] to-[hsl(340deg_100%_16%)]"></span>
                     <span class="block relative px-[27px] py-[12px] rounded-xl text-white bg-[hsl(345deg_100%_47%)] will-change-transform translate-y-[-4px] transition-transform duration-600 ease-[cubic-bezier(0.3,0.7,0.4,1)] group-hover:translate-y-[-6px] group-active:translate-y-[-2px] text-[1.1rem]">
