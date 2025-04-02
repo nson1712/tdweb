ead2eefdfdc1812675f66032e0693994fea8be1f
ADPC <phamngocson171201@gmail.com>
2025-04-02 17:36:11 +0700

    fix: fix key warning

--- src/pages/Payment/components/DefaultDeposit.jsx
+++ src/pages/Payment/components/DefaultDeposit.jsx
@@ -79,9 +79,8 @@ const DefaultDeposit = () => {
 
                   <div className="mt-4 space-y-2">
                     {diamondPackage.map((item, index) => (
-                      <>
-                        <div
-                          key={index}
+                      <div key={index}>
+                        <div 
                           class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer transition duration-300"
                           onClick={() =>
                             setPreviewQr({ visible: true, qr: item.qr })
@@ -100,7 +99,7 @@ const DefaultDeposit = () => {
                             }}
                           />
                         )}
-                      </>
+                      </div>
                     ))}
                   </div>
                 </div>
