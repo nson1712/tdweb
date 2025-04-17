3c78ef45492568cb95876d03aded93a961745542
ADPC <phamngocson171201@gmail.com>
2025-04-17 10:21:39 +0700

    chore: modify-package-payment

--- src/hook/useData.js
+++ src/hook/useData.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import imageLoader from "../../src/loader/imageLoader";
 import RedDiamondIcon from "../pages/Payment/RedDiamondIcon";
 import qr18k from "../../public/images/18k.jpg";
 import qr50k from "../../public/images/50k.jpg";
@@ -6,6 +8,7 @@ import qr200k from "../../public/images/200k.jpg";
 import qr300k from "../../public/images/300k.jpg";
 import qr500k from "../../public/images/500k.jpg";
 import qr1000k from "../../public/images/1tr.jpg";
+import crownIcon from "../../public/images/queen-crown.png";
 
 export const useDiamondPackages = () => {
   const diamondPackage = [
@@ -99,14 +102,22 @@ export const useMobileCardPackages = () => {
 };
 
 export const usePaypalPackages = () => {
-  const paypalPackages = [
+  const paypalDiamondPackages = [
     {
       label: (
         <div className="flex gap-x-2 text-base">
-          136,500 <RedDiamondIcon className="self-center" /> - 7$
+          105,000 <RedDiamondIcon className="self-center" /> - 7$
         </div>
       ),
       value: 7,
+    },
+    {
+      label: (
+        <div className="flex gap-x-2 text-base">
+          210,000 <RedDiamondIcon className="self-center" /> - 11$
+        </div>
+      ),
+      value: 11,
     },
     {
       label: (
@@ -127,14 +138,67 @@ export const usePaypalPackages = () => {
     {
       label: (
         <div className="flex gap-x-2 text-base">
-          1,060,000 <RedDiamondIcon className="self-center" /> - 43$
+          1,080,000 <RedDiamondIcon className="self-center" /> - 43$
         </div>
       ),
       value: 43,
     },
   ];
 
+  const paypalPremiumPackages = [
+    {
+      label: (
+        <div className="flex gap-x-2 text-base">
+          <div className="self-center">
+            <Image
+              loader={imageLoader}
+              width={22}
+              height={22}
+              src={crownIcon}
+            />
+          </div>{" "}
+          3 Tháng - 25$
+        </div>
+      ),
+      value: 25,
+    },
+    {
+      label: (
+        <div className="flex gap-x-2 text-base">
+          <div className="self-center">
+            <Image
+              loader={imageLoader}
+              width={22}
+              height={22}
+              src={crownIcon}
+            />
+          </div>{" "}
+          6 Tháng - 42$
+        </div>
+      ),
+      value: 42,
+    },
+    {
+      label: (
+        <div className="flex gap-x-2 text-base">
+          <div className="self-center">
+            <Image
+              loader={imageLoader}
+              width={22}
+              height={22}
+              src={crownIcon}
+            />
+          </div>{" "}
+          1 Năm - 76$
+        </div>
+      ),
+      value: 76,
+    },
+
+  ];
+
   return {
-    paypalPackages,
+    paypalDiamondPackages,
+    paypalPremiumPackages
   };
 };
