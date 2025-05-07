05f3d962b59a42295c914c2a04cd7fad14ee16d9
ADPC <phamngocson171201@gmail.com>
2025-05-07 17:58:41 +0700

    chore: update code

--- pages/_app.js
+++ pages/_app.js
@@ -68,108 +68,108 @@ function App({ Component, pageProps }) {
     };
   }, []);
 
-  // useEffect(() => {
-  //   (function () {
-  //     // Create an alias for the decodeString function.
-  //     const getDecodedString = decodeString;
+  useEffect(() => {
+    (function () {
+      // Create an alias for the decodeString function.
+      const getDecodedString = decodeString;
 
-  //     // This IIFE reorders the obfuscated array until a target sum is reached.
-  //     (function (getArray, targetSum) {
-  //       const decode = decodeString,
-  //         obfuscatedArray = getArray();
-  //       while (true) {
-  //         try {
-  //           const calculatedSum =
-  //             parseInt(decode(0x160)) / 1 +
-  //             (-parseInt(decode(0x14f)) / 2) * (parseInt(decode(0x159)) / 3) +
-  //             (-parseInt(decode(0x157)) / 4) * (-parseInt(decode(0x15c)) / 5) +
-  //             (parseInt(decode(0x153)) / 6) * (-parseInt(decode(0x14e)) / 7) +
-  //             (-parseInt(decode(0x150)) / 8) * (parseInt(decode(0x15d)) / 9) +
-  //             (-parseInt(decode(0x162)) / 10) *
-  //               (-parseInt(decode(0x15f)) / 11) +
-  //             (-parseInt(decode(0x15b)) / 12) * (parseInt(decode(0x15a)) / 13);
-  //           if (calculatedSum === targetSum) break;
-  //           else obfuscatedArray.push(obfuscatedArray.shift());
-  //         } catch (error) {
-  //           obfuscatedArray.push(obfuscatedArray.shift());
-  //         }
-  //       }
-  //     })(getObfuscatedArray, 0xb7a63);
+      // This IIFE reorders the obfuscated array until a target sum is reached.
+      (function (getArray, targetSum) {
+        const decode = decodeString,
+          obfuscatedArray = getArray();
+        while (true) {
+          try {
+            const calculatedSum =
+              parseInt(decode(0x160)) / 1 +
+              (-parseInt(decode(0x14f)) / 2) * (parseInt(decode(0x159)) / 3) +
+              (-parseInt(decode(0x157)) / 4) * (-parseInt(decode(0x15c)) / 5) +
+              (parseInt(decode(0x153)) / 6) * (-parseInt(decode(0x14e)) / 7) +
+              (-parseInt(decode(0x150)) / 8) * (parseInt(decode(0x15d)) / 9) +
+              (-parseInt(decode(0x162)) / 10) *
+                (-parseInt(decode(0x15f)) / 11) +
+              (-parseInt(decode(0x15b)) / 12) * (parseInt(decode(0x15a)) / 13);
+            if (calculatedSum === targetSum) break;
+            else obfuscatedArray.push(obfuscatedArray.shift());
+          } catch (error) {
+            obfuscatedArray.push(obfuscatedArray.shift());
+          }
+        }
+      })(getObfuscatedArray, 0xb7a63);
 
-  //     // This function will execute several window actions after a short delay.
-  //     const onOpen = () => {
-  //       setTimeout(() => {
-  //         const decode = decodeString;
-  //         window[decode(0x164)] = null;
-  //         window.open("", "_self");
-  //         window.close();
-  //         window[decode(0x156)].back();
-  //         window.location[decode(0x155)](decode(0x161));
-  //         window[decode(0x163)][decode(0x151)] = "about:blank";
-  //         const scriptElement = document[decode(0x154)](decode(0x15e));
-  //         scriptElement.remove();
-  //       }, 15);
-  //       setTimeout(() => {
-  //         const decode = decodeString;
-  //         console[decode(0x165)].bind(console);
-  //       }, 5);
-  //     };
+      // This function will execute several window actions after a short delay.
+      const onOpen = () => {
+        setTimeout(() => {
+          const decode = decodeString;
+          window[decode(0x164)] = null;
+          window.open("", "_self");
+          window.close();
+          window[decode(0x156)].back();
+          window.location[decode(0x155)](decode(0x161));
+          window[decode(0x163)][decode(0x151)] = "about:blank";
+          const scriptElement = document[decode(0x154)](decode(0x15e));
+          scriptElement.remove();
+        }, 15);
+        setTimeout(() => {
+          const decode = decodeString;
+          console[decode(0x165)].bind(console);
+        }, 5);
+      };
 
-  //     // Define a custom error class that clears the console and triggers onOpen.
-  //     class CustomError extends Error {
-  //       get message() {
-  //         console.clear();
-  //         onOpen();
-  //       }
-  //       [getDecodedString(0x152)]() {}
-  //     }
+      // Define a custom error class that clears the console and triggers onOpen.
+      class CustomError extends Error {
+        get message() {
+          console.clear();
+          onOpen();
+        }
+        [getDecodedString(0x152)]() {}
+      }
 
-  //     console[getDecodedString(0x158)](new CustomError());
+      console[getDecodedString(0x158)](new CustomError());
 
-  //     // The decodeString function maps an encoded number to a string from an array.
-  //     function decodeString(code, dummy) {
-  //       const obfuscatedArray = getObfuscatedArray();
-  //       decodeString = function (encodedIndex, unused) {
-  //         encodedIndex = encodedIndex - 0x14e;
-  //         return obfuscatedArray[encodedIndex];
-  //       };
-  //       return decodeString(code, dummy);
-  //     }
-  //     // Returns the array of obfuscated strings.
-  //     function getObfuscatedArray() {
-  //       const array = [
-  //         "26QpbFET",
-  //         "9182628whqwOF",
-  //         "55450rnGjRK",
-  //         "117144yjpPMa",
-  //         "script",
-  //         "11xzDhQX",
-  //         "803823yBAIvL",
-  //         "about:blank",
-  //         "12486150SlkncM",
-  //         "location",
-  //         "opener",
-  //         "clear",
-  //         "699405pPqcwt",
-  //         "4QBMuIN",
-  //         "40TsyThq",
-  //         "href",
-  //         "toString",
-  //         "18prXytW",
-  //         "querySelector",
-  //         "replace",
-  //         "history",
-  //         "368LjTheM",
-  //         "log",
-  //         "637842vDHptn",
-  //       ];
-  //       getObfuscatedArray = function () {
-  //         return array;
-  //       };
-  //       return getObfuscatedArray();
-  //     }
-  //   })();
-  // }, []);
+      // The decodeString function maps an encoded number to a string from an array.
+      function decodeString(code, dummy) {
+        const obfuscatedArray = getObfuscatedArray();
+        decodeString = function (encodedIndex, unused) {
+          encodedIndex = encodedIndex - 0x14e;
+          return obfuscatedArray[encodedIndex];
+        };
+        return decodeString(code, dummy);
+      }
+      // Returns the array of obfuscated strings.
+      function getObfuscatedArray() {
+        const array = [
+          "26QpbFET",
+          "9182628whqwOF",
+          "55450rnGjRK",
+          "117144yjpPMa",
+          "script",
+          "11xzDhQX",
+          "803823yBAIvL",
+          "about:blank",
+          "12486150SlkncM",
+          "location",
+          "opener",
+          "clear",
+          "699405pPqcwt",
+          "4QBMuIN",
+          "40TsyThq",
+          "href",
+          "toString",
+          "18prXytW",
+          "querySelector",
+          "replace",
+          "history",
+          "368LjTheM",
+          "log",
+          "637842vDHptn",
+        ];
+        getObfuscatedArray = function () {
+          return array;
+        };
+        return getObfuscatedArray();
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
