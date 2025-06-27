87946b38387607178cb9171a6ea87b28675c4128
khoitd <khoi.sm@ascertain.com.my>
2025-06-27 14:36:52 +0700

    update hashtag display

--- src/pages/Research/HashtagSection/index.jsx
+++ src/pages/Research/HashtagSection/index.jsx
@@ -9,14 +9,14 @@ const HashtagSection = ({ hashtags }) => {
       <div className="hidden md:block px-2">
         <div className="flex flex-wrap gap-4 justify-center">
           {hashtags?.data?.map((item, index) => (
-            <Hashtag key={item.id} hashtag={item.name} index={index}/>
+            <Hashtag key={item.id} hashtag={item} index={index}/>
           ))}
         </div>
       </div>
       <div className="block md:hidden px-1">
         <div className="flex flex-wrap gap-2 justify-center">
           {hashtags?.data?.slice(0, 14).map((item, index) => (
-            <Hashtag key={item.id} hashtag={item.name} index={index}/>
+            <Hashtag key={item.id} hashtag={item} index={index}/>
           ))}
         </div>
       </div>
